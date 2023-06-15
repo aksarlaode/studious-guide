bae80007d27544390a95a91970cc5499eaceb4ac
aksarlaode <akhsaras21@gmail.com>
2023-06-15 01:05:54 +0000

    ingestions

--- packages/ui/src/icons.tsx
+++ packages/ui/src/icons.tsx
@@ -3,7 +3,9 @@ import type { Icon as LucideIcon } from "lucide-react";
 
 export type Icon = LucideIcon;
 
+export const Laptop = Lucide.Laptop;
 export const Logo = Lucide.Command;
+export const Dashboard = Lucide.Activity;
 export const Close = Lucide.X;
 export const Spinner = Lucide.Loader2;
 export const ChevronLeft = Lucide.ChevronLeft;
